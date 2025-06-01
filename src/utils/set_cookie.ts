4d79f7b010c4f10cbed61159a0d380319ad93f6d
RichardM20 <richardoficial2020@gmail.com>
2025-06-01 13:25:30 -0500

    fix: cookies set

--- src/utils/set_cookie.ts
+++ src/utils/set_cookie.ts
@@ -2,20 +2,24 @@ import { Response } from "express";
 import ENV from "../config/env.config";
 
 export const setTokenCookie = (res: Response, token: string) => {
+  const isProd = ENV.NODE_ENV === "production";
+
   res.cookie("token", token, {
     httpOnly: true,
-    secure: ENV.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
     path: "/",
     maxAge: 3600000,
   });
 };
 
 export function clearTokenCookie(res: Response) {
+  const isProd = process.env.NODE_ENV === "production";
+
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: isProd,
+    sameSite: isProd ? "none" : "strict",
     path: "/",
   });
 }
